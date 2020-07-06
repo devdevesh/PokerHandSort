e75ff1ecf8a1757c4e03efb1cca4ab5c581c4fcf
Devesh <deveshdev@live.com>
2020-07-06 12:53:46 +1000

    text change

--- PokerHandSort/Program.cs
+++ PokerHandSort/Program.cs
@@ -18,74 +18,73 @@ namespace PokerHandSort
             string textFile = Filename;
             if (File.Exists(textFile))
             {
-                Console.WriteLine("Has Document");
-            try
-            {   // Open the text file using a stream reader.
-                using (StreamReader br = new StreamReader(Filename))
-                {
-                    while (true)
+                try
+                {   // Open the text file using a stream reader.
+                    using (StreamReader br = new StreamReader(Filename))
                     {
-                        string input = br.ReadLine();
-                        if(string.ReferenceEquals(input,null))
+                        while (true)
                         {
-                            break;
+                            string input = br.ReadLine();
+                            if (string.ReferenceEquals(input, null))
+                            {
+                                break;
+                            }
+                            String check = "(?:[2-9TJQKA][SCHD] ){9}[2-9TJQKA][SCHD]";
+                            Match match = Regex.Match(input, check);
+                            if (match.Success == false)
+                            {
+                                Console.WriteLine("Input Format is Wrong.");
+                                break;
+                            }
+                            String[] cards = input.Split(" ");
+
+                            //Splitting action
+                            String[] handOneStr = cards.Take(5).ToArray();
+                            String[] handTwoStr = cards.Skip(5).ToArray();
+
+                            //Sorting
+                            Hand.Sorting(handOneStr);
+                            Hand.Sorting(handTwoStr);
+
+                            Hand handOne = new Hand(handOneStr);
+                            Hand handTwo = new Hand(handTwoStr);
+
+                            handOne.Evaluate();
+                            handTwo.Evaluate();
+
+                            //Checking who is a winner returns 1 is Player 1 wins
+
+                            int res = Hand.Winner(handOne, handTwo);
+                            if (res == 1)
+                            {
+                                winsPlayer1++;
+                            }
+                            else if (res == 2)
+                            {
+                                winsPlayer2++;
+                            }
+                            else
+                            {
+                                Console.WriteLine(TIE);
+                            }
+
                         }
-                        String check = "(?:[2-9TJQKA][SCHD] ){9}[2-9TJQKA][SCHD]";
-                        Match match = Regex.Match(input, check);
-                        if (match.Success == false)
-                        {
-                            Console.WriteLine("Input Format is Wrong.");
-                            break;
-                        }
-                        String[] cards = input.Split(" ");
-
-                        //Splitting action
-                        String[] handOneStr = cards.Take(5).ToArray();
-                        String[] handTwoStr = cards.Skip(5).ToArray();
-
-                        //Sorting
-                        Hand.Sorting(handOneStr);
-                        Hand.Sorting(handTwoStr);
-                                                
-                        Hand handOne = new Hand(handOneStr);
-                        Hand handTwo = new Hand(handTwoStr);
-
-                        handOne.Evaluate();
-                        handTwo.Evaluate();
-
-                        //Checking who is a winner returns 1 is Player 1 wins
-
-                        int res = Hand.Winner(handOne, handTwo);
-                        if (res == 1)
-                        {
-                            winsPlayer1++;
-                        }
-                        else if (res == 2)
-                        {
-                            winsPlayer2++;
-                        }
-                        else
-                        {
-                            Console.WriteLine(TIE);
-                        }
-
+                        Console.WriteLine("Player 1: " + winsPlayer1 + " hands");
+                        Console.WriteLine("Player 2: " + winsPlayer2 + " hands");
+                        Console.WriteLine("\nPress any key to Exit...");
+                        Console.ReadKey();
                     }
-                    Console.WriteLine("Player 1: " + winsPlayer1 + " hands");
-                    Console.WriteLine("Player 2: " + winsPlayer2 + " hands");
-                    Console.WriteLine("\nPress any key to Exit...");
-                    Console.ReadKey();                
                 }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
+                catch (IOException e)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }
 
             }
             else
             {
-                Console.WriteLine("The Text file that you input dosen't exist");
+                Console.WriteLine("The text file that you input dosen't exist");
             }
                
         }
